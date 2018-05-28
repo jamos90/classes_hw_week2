8911e0bc751e4c6d340b7821fbb8d4375dfd2b97
jamos90 <jamos_98@hotmail.com>
2018-05-28 15:55:51 +0100

    all tests passed

--- hw_day01.rb
+++ hw_day01.rb
@@ -9,6 +9,26 @@ class Student
     return @student_name
   end
 
+  def student_cohort()
+    return @cohort
+  end
+
+  def set_student_name(new_student)
+    @student_name = new_student
+  end
+
+  def set_cohort(new_cohort)
+    @cohort = new_cohort
+  end
+
+  def student_talk()
+    return "I can talk"
+  end
+
+  def favourite_language()
+    return "I love Ruby!"
+  end
+
 
 
 
--- specs/hw_day01_specs.rb
+++ specs/hw_day01_specs.rb
@@ -8,4 +8,36 @@ class StudentTest < MiniTest::Test
     assert_equal("James",student.student_name())
   end
 
+  def test_student_cohort
+    student  = Student.new("James", "E22")
+    assert_equal("E22", student.student_cohort())
+  end
+
+  def test_set_student_name
+    student = Student.new("James", "E22")
+    student.set_student_name("Simon")
+    assert_equal("Simon", student.student_name())
+  end
+
+  def test_set_cohort
+    student = Student.new("James", "E22")
+    student.set_cohort("E21")
+    assert_equal("E21", student.student_cohort())
+  end
+
+  def test_student_talk
+    student = Student.new("James", "E22")
+    assert_equal("I can talk", student.student_talk())
+  end
+
+  def test_favourite_language
+    student = Student.new("James", "E22")
+    assert_equal("I love Ruby!", student.favourite_language())
+  end
+
+
+
+
+
+
 end
